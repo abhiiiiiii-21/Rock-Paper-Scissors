8e4673161c614b51f534f36f2bcc56eb503778d6
Abhishek Kumar Patel <abhishekpatel12131@gmail.com>
2025-02-10 23:10:26 +0530

    ongoing

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 window.addEventListener("load", function () {
     document.getElementById("preloader").style.display = "none";
     document.getElementById("content").style.display = "block";
+  });
+  
+const gameContainer = document.querySelector(".container");
+const userResult = document.querySelector(".you img");
+const botResult = document.querySelector(".computer img");
+const result = document.querySelector(".situation p");
+const optionImages = document.querySelectorAll(".option");
+
+
+optionImages.forEach((image,index) => {
+
+    image.addEventListener("click", (e) => {
+        image.classList.add("active")
+
+        optionImages.forEach((image2,index2) => {
+            index !== index2 && image2.classList.remove("active")
+        });  
+
+        let imageSrc = e.target.querySelector("img").src
+        userResult.src = imageSrc
+
+        let randomNum = Math.floor(Math.random()* 3)
+        let computerImages = ["./images/rock.png","./images/paper.png","./images/scissor.png"]
+        botResult.src = computerImages[randomNum]
+                
+    })
 });
-
-const options = document.querySelectorAll(".option");
-const userChoiceImg = document.getElementById("userChoice");
-const botChoiceImg = document.getElementById("botChoice");
-const resultText = document.getElementById("result");
-
-const choices = ["rock", "paper", "scissors"];
-
-
